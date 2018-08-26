4056ac93546acf70675592da63af3f334dbd631e
Federico Silvestri <federico.silvestri@divisibleodd.com>
2018-08-26 16:10:58 +0200

    Reduces stress test for runner.

--- scripts/test_project.sh
+++ scripts/test_project.sh
@@ -2,9 +2,12 @@
 
 echo "Testing Project" 
 
-for ((i = 0; i < 10; i++)); do
-    echo "Making test1 $i"
-    make test1
-done
+#for ((i = 0; i < 10; i++)); do
+#    echo "Making test1 $i"
+#    make test1
+#done
+
+echo "Making test1 $i"
+make test1
 
 echo "TEST PROJECT PASSED"
